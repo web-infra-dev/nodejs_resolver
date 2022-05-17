1d78a9454dd949248eef3f1d2d25976145311d5c
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-05-17 14:23:57 +0800

    feat: 🎸 try to use phf

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::time::Instant;
 use nodejs_resolver::Resolver;
 fn main() {
     let start = Instant::now();
-    for i in 0..100 {
+    for _ in 0..100 {
         Resolver::get_target_kind("react");
     }
 
-    for i in 0..100 {
-        Resolver::get_target_kind("fs");
+    for _ in 0..100 {
+        Resolver::get_target_kind("testtest");
     }
     println!("{:?}", start.elapsed());
 }
