67ad7f7379a18c4103ca0bf350d34f5baf98a631
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-05-18 14:20:53 +0800

    perf: ⚡️ smol_str instead of string

--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,12 @@
+use smol_str::SmolStr;
+
 use crate::Resolver;
 
 #[derive(Clone, Debug)]
 pub struct Request {
-    pub target: String,
-    pub query: String,
-    pub fragment: String,
+    pub target: SmolStr,
+    pub query: SmolStr,
+    pub fragment: SmolStr,
 }
 
 enum ParseStats {
@@ -23,24 +25,27 @@ impl Resolver {
         let mut stats = ParseStats::Start;
         for c in ident.chars() {
             match c {
-                '#' => {
-                    matches!(stats, ParseStats::Request | ParseStats::Query).then(|| {
+                '#' => match stats {
+                    ParseStats::Request | ParseStats::Query => {
                         stats = ParseStats::Fragment;
-                    });
-                    matches!(stats, ParseStats::Start).then(|| {
+                    }
+                    ParseStats::Start => {
                         stats = ParseStats::Request;
-                    });
-                }
-                '?' => {
-                    (!matches!(stats, ParseStats::Fragment)).then(|| {
+                    }
+                    ParseStats::Fragment => {}
+                },
+                '?' => match stats {
+                    ParseStats::Request | ParseStats::Query | ParseStats::Start => {
                         stats = ParseStats::Query;
-                    });
-                }
-                _ => {
-                    matches!(stats, ParseStats::Start).then(|| {
+                    }
+                    ParseStats::Fragment => {}
+                },
+                _ => match stats {
+                    ParseStats::Start => {
                         stats = ParseStats::Request;
-                    });
-                }
+                    }
+                    _ => {}
+                },
             };
             match stats {
                 ParseStats::Request => target.push(c),
@@ -55,9 +60,9 @@ impl Resolver {
     pub fn parse(target: &str) -> Request {
         let (target, query, fragment) = Self::parse_identifier(target);
         Request {
-            target,
-            query,
-            fragment,
+            target: target.into(),
+            query: query.into(),
+            fragment: fragment.into(),
         }
     }
 }
