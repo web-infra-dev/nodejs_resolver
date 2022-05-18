1a805a16d5cbbb9497f0eb31e43462a25e04a591
bvanjoi <bohan-zhang@foxmail.com>
2022-05-18 13:04:31 +0800

    docs: docs for `ResovlerOptions`

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ TodoList:
 
 - [ ] optimized code structure.
 
-## Options
-
 ## Benchmark
 
 In the [bench](./bench/README.md), compared the the performance along [esbuild(wasm)](https://github.com/evanw/esbuild), [enhanced-resolve](https://github.com/webpack/enhanced-resolve) and this project base on [ant-design](https://github.com/ant-design/ant-design)
--- src/options.rs
+++ src/options.rs
@@ -2,16 +2,48 @@ use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, Clone)]
 pub struct ResolverOptions {
+    /// Tried detect file with this extension.
+    /// Default is ["js", "json", "node"]
     pub extensions: Vec<String>,
+    /// Maps key to value.
+    /// `None` means that the value is `false`.
+    /// Default is `HashMap::new()`.
     pub alias: HashMap<String, Option<String>>,
-    pub condition_names: HashSet<String>,
-    pub symlinks: bool,
-    pub description_file: Option<String>,
+    /// The list of alias fields in description files.
+    /// TODO: currently only support one alias field.
+    /// Default is `[]`
     pub alias_fields: Vec<String>,
+    /// Condition names for exports filed. Note that its
+    /// type is a `HashSet`, because the priority is
+    /// related to the order in which the export field
+    /// fields are written.
+    /// Default is `Set(["node"])`.
+    pub condition_names: HashSet<String>,
+    /// Whether to resolve the real path when the result
+    /// is a symlink.
+    /// Default is `true`.
+    pub symlinks: bool,
+    /// A JSON file to describing this lib information.
+    /// Default is `Some("package.json")`. It can be set
+    /// to `None` when resolve css.
+    pub description_file: Option<String>,
+    /// Main file in this directory.
+    /// Default is `["index"]`.
     pub main_files: Vec<String>,
+    /// Main fields in Description.
+    /// Default is `["main"]`.
     pub main_fields: Vec<String>,
+    /// Directories to resolve module from.
+    /// Default is `["node_modules"]`.
     pub modules: Vec<String>,
+    /// Prefer to resolve request as relative request and
+    /// fallback to resolveing as modules.
+    /// Default is `false`
     pub prefer_relative: bool,
+    /// Cache had stored the processed `description_file` parsing information by default,
+    /// but the action is not secure, and when you try to modify a `description_file`,
+    /// they will still use the data before the modification.
+    /// Default is `true`.
     pub enable_unsafe_cache: bool,
 }
 
@@ -29,7 +61,8 @@ impl Default for ResolverOptions {
         let modules = vec![String::from("node_modules")];
         let symlinks = true;
         let alias_fields = vec![];
-        let condition_names: HashSet<String> = HashSet::new();
+        let condition_names: HashSet<String> =
+            HashSet::from_iter(["node"].into_iter().map(String::from));
         let prefer_relative = false;
         let enable_unsafe_cache = true;
         Self {
