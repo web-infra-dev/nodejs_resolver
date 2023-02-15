531e7875cf05268a68b46b696f803e23906fdcc8
Boshen <boshenc@gmail.com>
2023-02-15 09:19:21 +0800

    dbg

--- src/normalize.rs
+++ src/normalize.rs
@@ -11,11 +11,13 @@ impl NormalizePath for Path {
     #[inline]
     fn normalize(&self) -> Cow<Path> {
         // perf: this method does not re-allocate memory if the path does not contain any dots.
+        println!("{:?}", self);
         self.absolutize_from(Path::new("")).unwrap()
     }
 
     #[inline]
     fn normalized_eq(&self, other: &Path) -> bool {
+        println!("{:?} {:?}", self, other);
         self.normalize().eq(&other.normalize())
     }
 }
